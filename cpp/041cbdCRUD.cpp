24ff7c8f498863fc6f061912cf7b8f55b8c0996a
Marshall Abadeer <69655364+h-marshall69@users.noreply.github.com>
2024-05-10 15:57:32 -0500

    add 041

---
+++ cpp/041cbdCRUD.cpp
@@ -0,0 +1,30 @@
+// CRUD (create, Read, Update, Delete) con conexion a Base de datos
+// Libreria a utlizar: pip mysql-connector-python
+
+/*
+    CREATE DATABASE sisteams;
+    USE sistemas;
+    CREATE TABLE 'personas' (
+        'dni' varchar(8) NOT NULL,
+        'apellidopa' varchar(50) NOT NULL,
+        'apellidoma' varchar(50) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = utfmb4 COLLATE = utf8mb4_general_ci;
+
+    ALTER TABLE 'personas'
+        ADD PRIMARY KEY ('dni');
+    COMMIT;
+*/
+
+
+#include <iostream>
+#include <list>
+#include <cstdlib>
+#include <algorithm>
+#include <thread>
+#include <chrono>
+#include <map>
+using namespace std;
+
+int main() {
+    return 0;
+}
