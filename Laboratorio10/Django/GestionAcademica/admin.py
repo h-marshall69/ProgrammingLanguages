ecb2b214866b18ba999399bc55cddab7cb38785b
Marshall <h.jermaine.lee@gmail.com>
2024-07-21 18:07:15 -0300

    add laboratorio10

---
+++ Laboratorio10/Django/GestionAcademica/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from GestionAcademica.models import *
+# Register your models here.
+
+admin.site.register(Alumno)
+admin.site.register(Curso)
+admin.site.register(Matriculas)
